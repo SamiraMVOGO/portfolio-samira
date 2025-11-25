d68b2f1eb9c0ac24a678b44c875a4ec9aee27ea1
SamiraMVOGO <samira.mvogo@epitech.eu>
2025-11-25 13:07:09 +0100

    feat: add more project

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -61,13 +61,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className=" bg-[#181818] w-[250px] h-[250px] overflow-hidden rounded-full lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/sam.jpg"
-              alt="Sam image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              src="/images/samira.jpg"
+              alt="Samira image"
+              className="w-full absolute mt-20  transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={800}
+              height={800}
             />
           </div>
         </motion.div>
